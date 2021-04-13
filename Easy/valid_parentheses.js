cba57da45e6d53c26501b9d2a6be4b8f6231d15c
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-04-13 08:25:25 -0700

    add solution

---
+++ Easy/valid_parentheses.js
@@ -0,0 +1,33 @@
+/* Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false */
+
+var isValid = function(s) {
+    while (
+      s.indexOf("{}") !== -1 ||
+      s.indexOf("[]") !== -1 ||
+      s.indexOf("()") !== -1
+    ) {
+      s = s.replace("()", "");
+      s = s.replace("{}", "");
+      s = s.replace("[]", "");
+    }
+    return s === "";
+  };
