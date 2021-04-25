ffa981144f51af16b9088b66ab74d51eaf6e718b
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-04-25 08:23:40 -0700

    add solutions

---
+++ Easy/destination_city.js
@@ -0,0 +1,18 @@
+/* You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+
+
+Example 1:
+
+Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+Output: "Sao Paulo"
+Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo". */
+
+var destCity = function(paths) {
+    const arrivals = paths.map(a => a[0]);
+    const destinations =  paths.map(a => a[1]);
+
+    return destinations.filter(d => arrivals.indexOf(d) === -1).join('')
+};
