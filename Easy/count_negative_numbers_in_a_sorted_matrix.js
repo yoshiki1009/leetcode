8434d853730f0eab530b336afd294d088a7306d5
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-05-07 08:32:32 -0700

    add solutions

---
+++ Easy/count_negative_numbers_in_a_sorted_matrix.js
@@ -0,0 +1,18 @@
+/* Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+Example 1:
+
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+
+Input: grid = [[3,2],[1,0]]
+Output: 0
+ */
+
+var countNegatives = function(grid) {
+    let ans =0
+  grid.map(num =>num.map(n => n < 0 ? ans++ : ans))
+    return ans
+};
